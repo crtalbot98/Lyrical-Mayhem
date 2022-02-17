23b72f9c7ec5f2788dd46682b2444f765c56d843
Chandler Talbot <chandler.talbot@card.com>
2022-02-17 14:45:27 -0500

    moved npm modules and code to chunks

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,13 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const DotEnv = require('dotenv-webpack');
 
 module.exports = {
-  entry: ['./build/app.js', './src/assets/style/app.css'],
+  entry: {
+    app: path.resolve(__dirname, './build/app.js'), 
+    login: path.resolve(__dirname, './build/auth/login.js')
+  },
   mode: 'development',
   devtool: false,
   module: {
@@ -16,9 +20,27 @@ module.exports = {
     ]
   },
   plugins: [
+    new DotEnv(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Lyrical Mayhem'
+      inject: 'body',
+      template: './src/views/index.html',
+      filename: 'index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      },
+      chunks: ['app']
+    }),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './src/views/login.html',
+      filename: 'login.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      },
+      chunks: ['login']
     }),
     new CopyWebpackPlugin({
         patterns: [
@@ -27,7 +49,31 @@ module.exports = {
     })
   ],
   output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash:8].js'
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.es.js', '.css'],
+    fallback: {
+      'path': false,
+      'fs': false
+    }
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `npm.${packageName.replace('@', '')}`;
+          }
+        }
+      }
+    }
   }
 };
