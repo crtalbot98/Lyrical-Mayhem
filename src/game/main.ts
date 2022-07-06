10fcb871b2d7a6dd438d497aeef0eb6aff8d5eb9
Chandler Talbot <crtalbot98@gmail.com>
2022-07-06 16:04:20 -0400

    Removed controller from main

--- src/game/main.ts
+++ src/game/main.ts
@@ -2,9 +2,9 @@ import * as PIXI from "pixi.js";
 import Bullet from './entities/bullet';
 import Lyric from './entities/lyric';
 import Player from './entities/player';
-import LyricHandler from './lyricHandler';
+import LyricHandler from './objectHandlers/lyricHandler';
 import { detectCollisions } from './utils/collisions';
-import Controller from "./controller";
+import PlayerController from "./playerController";
 import { store } from '../stores/store';
 
 export default class Game {
@@ -12,20 +12,15 @@ export default class Game {
     private _app: PIXI.Application;
     private _player: Player;
     private _lyricHandler = new LyricHandler();
-    private _controller: Controller;
 
     constructor(){
         PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
         this._app = new PIXI.Application({ 
             resizeTo: window,
-            backgroundColor: 0x737373
+            backgroundColor: 0x1F2C28
         });
-        this._player = new Player({ 
-            x: this._app.renderer.width / 2, 
-            y: this._app.renderer.height / 2 
-        });
-        this._controller = new Controller(this._player)
+        this._player = new Player(new PlayerController());
     }
 
     public init(): void {
@@ -33,35 +28,28 @@ export default class Game {
         
         document.body.appendChild(this._app.view);
 
-        this._player.create();
-        this._controller.initListeners(stage);
-
-        stage.addChild(this._player.entity);
+        this._player.create(stage);
 
         this._app.ticker.add((delta: number) => {
             if(!store.getState().spotifyPlayer.playing) return;
-            this._lyricHandler.addLyric(stage);
-            this._controller.update(delta);
+            this._lyricHandler.update(stage, delta);
             this._player.update(delta, stage);
-            this.checkCollisions(delta)
+            // this.checkCollisions()
         });
     }
 
-    private checkCollisions(delta: number): void {
-        this._lyricHandler.lyrics.forEach((lyric: Lyric) => {
-            if(lyric.destroyed) this._app.stage.removeChild(lyric.entity);
-            else lyric.update(delta);
+    // private checkCollisions(): void {
+    //     this._lyricHandler.objects.forEach((lyric: Lyric) => {
+    //         this._player.bullets.forEach((bullet: Bullet) => {
+    //             if(lyric.destroyed && bullet.destroyed) return;
 
-            if(this._player.bullets.length >= 1) {
-                this._player.bullets.forEach((bullet: Bullet) => {
-                    if(detectCollisions(bullet.entity, lyric.entity)){
-                        lyric.destroyed = true;
-                        bullet.destroyed = true
-                    }
-                })
-            }
-        });
-    }
+    //             if(detectCollisions(bullet.entity, lyric.entity)){
+    //                 lyric.destroyed = true;
+    //                 bullet.destroyed = true
+    //             }
+    //         })
+    //     });
+    // }
 
     get lyricHandler(): LyricHandler {
         return this._lyricHandler
