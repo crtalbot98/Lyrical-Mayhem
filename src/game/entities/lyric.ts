796c754bfdc5ff8ba97d3034e0f1c2842db1d644
Chandler Talbot <crtalbot98@gmail.com>
2022-08-08 12:52:40 -0400

    Added entityMover class for moving and updating the player and moveable entities

--- src/game/entities/lyric.ts
+++ src/game/entities/lyric.ts
@@ -1,42 +1,48 @@
-import { Text } from "pixi.js";
+import { Text, TextMetrics, TextStyle } from "pixi.js";
 import { SimpleVector2D } from "src/types";
+import EntityMover from "../controllers/entityMover";
 import { PixiApp } from "../main";
 import { withinScreenBounds } from "../utils/collisions";
+import Vector2D from "../vector2D";
+import { Entity, MoveableEntity } from "./entity";
 
-export default class Lyric {
+export default class Lyric implements MoveableEntity {
 
-    private _fontStyles: {} = { fontFamily : 'Arial', fontSize: 24, fill : 0xDAF7DC, align : 'center' };
-    private _speed: number;
-    _text: string;
+    private _fontStyles: TextStyle;
+    _metrics: TextMetrics;
+    _speed: number;
     _entity: Text;
     _destroyed: boolean;
+    _direction: Vector2D;
+    _entityMover: EntityMover;
 
-    constructor(text: string, position: SimpleVector2D) {
-        this._destroyed = false;
+    constructor() {
         this._speed = 2;
-        this._text = text;
-        this._entity = new Text(this._text, this._fontStyles)
-        this._entity.position.x = position.x;
-        this._entity.position.y = position.y
+        this._entityMover = new EntityMover();
+        this._fontStyles = new TextStyle({ 
+            fontFamily : 'Arial', 
+            fontSize: 24, 
+            fill: 0xDAF7DC, 
+            align : 'center'
+        });
+        this._entity = new Text('', this._fontStyles);
+        this._direction = new Vector2D(0,1);
     }
 
-    public create(): void {
+    public create(text: string, position: SimpleVector2D): void {
+        this._destroyed = false;
+        this._metrics = TextMetrics.measureText(text, this._fontStyles);
+        this._entity.text = text;
+        this._entity.position.x = position.x;
+        this._entity.position.y = position.y;
         PixiApp.stage.addChild(this._entity)
     }
 
     public update(delta: number): void {
-        if(this._destroyed) {
-            PixiApp.stage.removeChild(this._entity);
-            return
+        if(this._destroyed || !withinScreenBounds(this._entity)) {
+            this._destroyed = true;
+            PixiApp.stage.removeChild(this._entity)
         }
-
-        if(!withinScreenBounds(this._entity)) this._destroyed = true;
-        this._entity.position.y += this._speed * delta;
-    }
-
-    public reset(position: SimpleVector2D): void {
-        this._destroyed = false;
-        this._entity.position.x = position.x;
-        this._entity.position.y = position.y
+        else this._entityMover.move(this, delta)
     }
 }
