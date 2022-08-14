b01ef4de8b6c5df8c309134556723f9564c08caa
Chandler Talbot <crtalbot98@gmail.com>
2022-08-14 18:53:59 -0400

    added loading on scroll

--- src/components/pause-menu/tracklist.tsx
+++ src/components/pause-menu/tracklist.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { GenericObject } from '../../types';
-import { useMenuContext } from './context';
+import { useMenuContext } from './menu';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../stores/store';
 import { usePlayerDevice } from 'react-spotify-web-playback-sdk';
+import { getPlaylistById } from './playlist';
 
 const Tracklist: React.FC = () => {
 	const aToken = useSelector((state: RootState) => state.auth.accessToken);
 	const device = usePlayerDevice();
 	const context = useMenuContext();
 	const dispatch = useDispatch();
+	const selectedPlaylist = getPlaylistById(context.playlists, context.selectedPlaylistId);
+	const [offset, setOffset] = useState(0);
+	const [trackList, setTrackList] = useState([]);
+	const [loading, setLoading] = useState(true);
 
 	const playAndSetTrack = async(track: GenericObject) => {
 		await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device.device_id}`, {
@@ -28,6 +33,30 @@ const Tracklist: React.FC = () => {
 		}})
 	}
 
+	const getTracksOnScroll = async(evt: React.UIEvent<HTMLUListElement, UIEvent>) => {
+		if(evt.target.scrollTop !== evt.target.scrollHeight - evt.target.offsetHeight) return;
+
+		setOffset(offset + 20);
+		getTracks()
+	}
+
+	const getTracks = async() => {
+		if(!selectedPlaylist) {
+			setTrackList([]);
+			return
+		}
+		setLoading(true);
+		const tracks = await fetch(selectedPlaylist.tracks.href + '?offset=' + (trackList.length + offset), {
+			headers: new Headers({
+				'Authorization': `Bearer ${aToken}`
+			})
+		});
+		const tracksJson = await tracks.json();
+		
+		setTrackList([...trackList, ...tracksJson.items]);
+		setLoading(false);
+	}
+
 	const getLyrics = async(songName: string, artist: string) => {
     const newLyrics = await fetch(`http://localhost:8888/lyrics/getSongLyrics?songName=${songName}&artistName=${artist}`);
     const newLyricsJson = await newLyrics.json();
@@ -43,22 +72,43 @@ const Tracklist: React.FC = () => {
     }});
   }
 
-	const tracklistItems = context.trackList.map((item: GenericObject) => {
+	const tracklistItems = trackList.map((item: GenericObject) => {
+		let seconds: string|number = Math.floor((item.track.duration_ms / 1000) % 60);
+  	let minutes: string|number = Math.floor((item.track.duration_ms / 1000 / 60));
+
+		if(seconds < 10) seconds = '0' + seconds;
+
 		return <li 
+			className='flex space-x-2 hover:border-orange w-full'
       key={item.track.id} 
       onClick={() => { 
 				playAndSetTrack(item.track);
 				getLyrics(item.track.name, item.track.artists[0].name)
       }}
     >
-			<p>{item.track.name}</p>
-			<p className='text-white'>{item.track.artists[0].name}</p>
+			<img className='h-auto' height='45' width='45' src={item.track.album.images[2].url} alt={`${item.track.name} album art`} />
+			<p className='flex flex-col'>
+				{item.track.name}
+				<span className='text-white'>{item.track.artists[0].name}</span>
+			</p>
+			<p>{item.track.album.name}</p>
+			<p>{`${minutes}:${seconds}`}</p>
 		</li>
 	});
 
-  return <ul className='overflow-y-auto max-h-96'>
-		{context.trackList.length > 0 ? tracklistItems : null}
-	</ul>
+	useEffect(() => {
+		getTracks()
+	}, [selectedPlaylist]);
+
+  return <>
+		<ul 
+			className='overflow-y-auto overflow-x-hidden space-y-2 max-h-96 animate-slideOpen'
+			onScroll={getTracksOnScroll}
+		>
+			{ tracklistItems }
+		</ul>
+		{ loading ? <div className='border-md h-14 w-14 animate-spin'></div> : null }
+	</>
 };
 
 export default Tracklist;
