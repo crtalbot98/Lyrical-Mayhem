f0af8ab77119a1d7ce194979070e667ff5d71ef7
Chandler Talbot <crtalbot98@gmail.com>
2022-08-14 18:53:44 -0400

    Moved menu context to menu component

--- src/components/pause-menu/menu.tsx
+++ src/components/pause-menu/menu.tsx
@@ -1,29 +1,49 @@
 import React, { useState, memo } from 'react'
 import FullScreenModal from '../modals/fullscreenModal';
-import { MenuContext, MenuContextType } from './context';
-import Playlist from './playlist';
+import { PlayList } from './playlist';
 import Tracklist from './tracklist';
+import { createContext, useContext } from 'react';
+
+export type MenuContextType = {
+  playlists: any[], 
+	setPlaylists: React.Dispatch<React.SetStateAction<any[]>>,
+	selectedPlaylistId: string,
+	setSelectedPlaylistId: React.Dispatch<React.SetStateAction<string>>
+}
+
+export const MenuContext = createContext<MenuContextType>({
+	playlists: [],
+	setPlaylists: () => {},
+	selectedPlaylistId: '',
+	setSelectedPlaylistId: () => {}
+});
+
+export const useMenuContext = () => {
+  const context = useContext(MenuContext);
+  if(!context) throw new Error('Warning! Extracting context without wrapping your component.');
+  return context
+}
 
 export interface CompoundedMenu extends React.FC{
-	PlayList: typeof Playlist,
+	PlayList: typeof PlayList,
 	TrackList: typeof Tracklist
 }
 
 const Menu: CompoundedMenu = ({children}) => {
-	const [trackList, setTrackList] = useState([]);
 	const [playlists, setPlaylists] = useState([]);
+	const [selectedPlaylistId, setSelectedPlaylistId] = useState('');
 
 	return <MenuContext.Provider 
-		value={{ trackList, setTrackList, playlists, setPlaylists } as MenuContextType}
+		value={{ playlists, setPlaylists, selectedPlaylistId, setSelectedPlaylistId } as MenuContextType}
 	>
-		<FullScreenModal classes={'bg-lightGray flex flex-col 2xl:flex-row justify-start p-10'}>
+		<FullScreenModal classes={'bg-lightGray grid grid-cols-1 auto-rows-min justify-start p-6 h-7/8'}>
 			<h1 className='text-orange'>Lyrical Mayhem</h1>
 			{children}
 		</FullScreenModal>
 	</MenuContext.Provider>
 };
 
-Menu.PlayList = memo(Playlist);
+Menu.PlayList = memo(PlayList);
 Menu.TrackList = memo(Tracklist);
 
 export default Menu;
